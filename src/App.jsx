99cdd980d0b33ce9acd540f637569b6646363a59
Apichat Wongtala <apichat26870@gmail.com>
2024-05-01 03:39:59 +0700

    two commit file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="mx-auto max-w-6xl grid grid-cols-[40%_60%] gap-6">
+    <div className="mt-12 mx-auto max-w-6xl grid grid-cols-[40%_60%] gap-6">
       <div>
-        <div className="sticky top-0 grid grid-rows-[35%_45%_20%] h-screen">
-        <div>Title</div>
+        <div className="sticky top-12 grid grid-rows-[35%_45%_20%] h-screen">
+        <div>
+          <div className="text-3xl text-primaryTitle">Apichat Wongtala</div>
+          <div>Junoi Front-end Developer *between jobs</div>
+          <div>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
+          <div>View Resume</div>
+        </div>
         <div>Navbar</div>
         <div className="flex items-end">Link</div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryTitle: "#127369",
+        primaryContent: "#4C5958",
+        primarySubcontent: "#8AA6A3",
+        primaryBase: "#4C5958",
+        primaryAccent: "#10403B",
+        primaryBg: "#BFBFBF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
