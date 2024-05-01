722bff8da0944e8d439fda321585cf7f9bdad47f
Apichat Wongtala <apichat26870@gmail.com>
2024-05-02 02:01:47 +0700

    add: section experience to commit file

---
+++ src/sections/Experience/Experience.jsx
@@ -0,0 +1,169 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import imgExp1 from "../../assets/exp_site.png";
+
+const Experience = () => {
+  const [isMouseEnter, setIsMouseEnter] = useState({});
+
+  return (
+    <div className="">
+      <div className="text-primaryAccent font-medium">Experience</div>
+
+      <div
+        className={`grid grid-cols-[30%_70%] rounded-md px-2 py-6 transition-all ${
+          isMouseEnter["id1"] ? " bg-zinc-900" : ""
+        }`}
+        onMouseEnter={() => setIsMouseEnter({ id1: true })}
+        onMouseLeave={() => setIsMouseEnter({ id1: false })}
+      >
+        <div>
+          <div>
+            <span
+              className={`text-sm ${
+                isMouseEnter["id1"] ? "text-primaryAccent" : ""
+              }`}
+            >
+              2023-2024
+            </span>
+          </div>
+          <div>
+            <img
+              src={imgExp1}
+              alt="imgExperience"
+              className="w-5/6 rounded-md border-2 border-gray-700"
+            />
+          </div>
+        </div>
+        <div className="grid gap-y-4">
+          <a href="https://janoibjr21.github.io/portfolioshow/" target="_blank">
+            <div
+              className={`text-primaryAccent ${
+                isMouseEnter["id1"] ? "text-primaryTitle" : ""
+              }`}
+            >
+              Portfolio of Apichat Wongtala
+            </div>
+          </a>
+          <div
+            className={`flex gap-4 items-end text-xl ${
+              isMouseEnter["id1"] ? "text-primaryTitle" : ""
+            }`}
+          >
+            <a
+              href="https://github.com/JanoiBJR21/portfolioshow"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="hover:scale-125 hover:text-primaryTitle transition-all"
+              />
+            </a>
+          </div>
+          <div className="text-sm">
+            · A simple and minimalist portfolio for developers built with React
+            and TailwindCSS, designed to be clean and fast.
+          </div>
+          <div className="flex gap-4 text-sm">
+            <a href="https://react.dev/" target="_blank">
+              <div
+                className={`bg-zinc-800 px-2 py-1 rounded-md ${
+                  isMouseEnter["id1"] ? "text-primaryTitle" : ""
+                }`}
+              >
+                React
+              </div>
+            </a>
+            <a href="https://tailwindcss.com/" target="_blank">
+              <div
+                className={`bg-zinc-800 px-2 py-1 rounded-md ${
+                  isMouseEnter["id1"] ? "text-primaryTitle" : ""
+                }`}
+              >
+                TailwindCSS
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`grid grid-cols-[30%_70%] rounded-md px-2 py-6 transition-all ${
+          isMouseEnter["id1"] ? " bg-zinc-900" : ""
+        }`}
+        onMouseEnter={() => setIsMouseEnter({ id1: true })}
+        onMouseLeave={() => setIsMouseEnter({ id1: false })}
+      >
+        <div>
+          <div>
+            <span
+              className={`text-sm ${
+                isMouseEnter["id1"] ? "text-primaryAccent" : ""
+              }`}
+            >
+              2023-2024
+            </span>
+          </div>
+          <div>
+            <img
+              src={imgExp1}
+              alt="imgExperience"
+              className="w-5/6 rounded-md border-2 border-gray-700"
+            />
+          </div>
+        </div>
+        <div className="grid gap-y-4">
+          <a href="https://janoibjr21.github.io/portfolioshow/" target="_blank">
+            <div
+              className={`text-primaryAccent ${
+                isMouseEnter["id1"] ? "text-primaryTitle" : ""
+              }`}
+            >
+              Portfolio of Apichat Wongtala
+            </div>
+          </a>
+          <div
+            className={`flex gap-4 items-end text-xl ${
+              isMouseEnter["id1"] ? "text-primaryTitle" : ""
+            }`}
+          >
+            <a
+              href="https://github.com/JanoiBJR21/portfolioshow"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="hover:scale-125 hover:text-primaryTitle transition-all"
+              />
+            </a>
+          </div>
+          <div className="text-sm">
+            · A simple and minimalist portfolio for developers built with React
+            and TailwindCSS, designed to be clean and fast.
+          </div>
+          <div className="flex gap-4 text-sm">
+            <a href="https://react.dev/" target="_blank">
+              <div
+                className={`bg-zinc-800 px-2 py-1 rounded-md ${
+                  isMouseEnter["id1"] ? "text-primaryTitle" : ""
+                }`}
+              >
+                React
+              </div>
+            </a>
+            <a href="https://tailwindcss.com/" target="_blank">
+              <div
+                className={`bg-zinc-800 px-2 py-1 rounded-md ${
+                  isMouseEnter["id1"] ? "text-primaryTitle" : ""
+                }`}
+              >
+                TailwindCSS
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
--- src/sections/RightSection/RightSection.jsx
+++ src/sections/RightSection/RightSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "../About/About";
+import Experience from "../Experience/Experience";
 
 const RightSection = () => {
   return (
-    <div className="grid gap-y-4 px-5">
+    <div className="grid gap-y-40 px-5">
       <About />
+      <Experience/>
       <div>Experience</div>
       <div>Experience</div>
       <div>Experience</div>
